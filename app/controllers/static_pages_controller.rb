e9177a702b60fbcbc52083c0ae0e2f2f02db9b47
Le Thi Cuc <le.thi.cuc@framgia.com>
2016-09-26 15:12:04 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
+    @users = User.paginate page: params[:page]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "signup" => "users#new"
-  get "login" => "sessions#new"
+  get "login" => "user#new"
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
   resources :users
