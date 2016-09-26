22be9e5d0e3f2119f2613974bf3dec57a1aa0bdd
Le Thi Cuc <le.thi.cuc@framgia.com>
2016-09-26 08:11:27 +0700

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, options = {size: 80})
+    size = options[:size]
+    image_tag("avatar.png", alt: user.name)
+  end
 end
